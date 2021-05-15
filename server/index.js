b3544942a2e40749c4ae37c8f32db4e02b474b35
kosuke-17 <tamutamu17@icloud.com>
2021-05-15 15:21:47 +0900

    socket使用して、サーバー接続

--- server/index.js
+++ server/index.js
@@ -4,8 +4,20 @@ const http =require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-server.listen(PORT, () => console.log(`Server has Started on port ${PORT}`));
+io.on('connecton', (socket) => {
+  console.log('新たな接続をします');
+
+  socket.on('disconnect', () => {
+    console.log('User has left(ユーザーが退出しました)')
+  })
+})
+
+app.use(router);
+
+server.listen(PORT, () => console.log(` ${PORT}番でサーバーが起動しています`));
